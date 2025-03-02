01ed2245f8e17645c2261bc82d3a310e83b6422d
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-03-02 13:36:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
+FIVESIM_API_KEY = os.getenv("FIVESIM_API_KEY")
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
+
+DB_NAME = "database.db"
