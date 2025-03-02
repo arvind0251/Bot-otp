650e0dd28607bd895bda4afdf705dad35e050c61
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-03-02 13:41:38 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
